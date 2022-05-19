76f6fc65eb4055083897b874186f8631e91bc539
ithesadson <thesadson@gmail.com>
2022-05-19 16:43:25 +0300

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import os
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/qrcode')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'qrcode.png', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+if __name__ == '__main__':
+   app.run()
